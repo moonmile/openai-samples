fad82697e5efb58ee698f98a9d6e77f105d2a069
Tomoaki Masuda <masuda@moonmile.net>
2023-12-01 16:48:55 +0900

    ch04のサンプル

---
+++ ch04/samp0401/Program.cs
@@ -0,0 +1,7 @@
+﻿using SharpToken;
+
+string text = "この文章のトークン数はいくつですか？";
+// string text = "How many tokens are in this sentence?";
+var encoding = GptEncoding.GetEncodingForModel("gpt-4");
+List<int> tokens = encoding.Encode(text);
+Console.WriteLine(tokens.Count);
